93a59960d32fe5073a0d45a047ce408262b2aee3
danielpdaniel <artofjquinn@gmail.com>
2023-06-06 10:26:57 -0400

    commit tree 159c728ccc9a570cff96083837a31cccd380a7cd

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,6 +1,21 @@
 def reverse_string(str)
   # type your code in here
+
+  # define variable for an array of characters using .chars method
+  # iterate over each character of string
+  # insert each character into array at the start of the array
+  # join array into new, reversed string
+  characters = str.chars
+  reversed_str_arr = []
+  characters.each do |char|
+    reversed_str_arr.insert(0, char)
+  end
+  reversed_str_arr.join
 end
+
+# testcases
+# "hello there"
+# "589(*(*&*))"
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
